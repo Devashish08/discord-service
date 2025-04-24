a3d5114d1e8eb5335526247e6baf58e78471070c
Devashish08 <devashish.cs025@gmail.com>
2025-04-24 19:33:56 +0000

    refactor(utils): Use named constant for Discord API member fetch limit

--- utils/constants.go
+++ utils/constants.go
@@ -2,12 +2,15 @@ package utils
 
 import "github.com/Real-Dev-Squad/discord-service/dtos"
 
-const NICKNAME_SUFFIX = "-Can't Talk"
-const NICKNAME_PREFIX = "🎧 "
+const (
+	DiscordGuildMembersAPILimit = 1000
+	NICKNAME_SUFFIX             = "-Can't Talk"
+	NICKNAME_PREFIX             = "🎧 "
+)
 
 var CommandNames = dtos.CommandNameTypes{
-	Hello:     "hello",
-	Listening: "listening",
-	Verify:    "verify",
+	Hello:       "hello",
+	Listening:   "listening",
+	Verify:      "verify",
 	MentionEach: "mention-each",
 }
